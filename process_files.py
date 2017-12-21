ed305b713e9408115dd5a3d747425b2f058268a4
jmbenlloch <info@jmbenlloch.net>
2017-12-21 13:14:04 +0100

    Add plotting to daqdell script

--- process_files.py
+++ process_files.py
@@ -1,6 +1,9 @@
 import argparse
 import os
 import json
+import tables as tb
+import sys
+from matplotlib import pylab as plt
 
 from subprocess import call
 
@@ -34,3 +37,35 @@ with open(config_file, 'w') as outfile:
 cmd = 'export PATH=/home/dateuser/miniconda/bin:$PATH; source /home/dateuser/rawdata/manage.sh env; /home/dateuser/rawdata/decode {0}'.format(config_file)
 print(cmd)
 call(cmd, shell=True, executable='/bin/bash')
+
+
+def plot_waveform(waveforms, sensors):
+    nevts, nsensors, _ = waveforms.shape
+    for evt in range(nevts):
+        for s in range(nsensors):
+            data = waveforms[evt, s, :]
+            ymin = min(data)
+            ymax = max(data)
+            ymin = ymin - 0.1 * ymin
+            ymax = ymax + 0.1 * ymax
+
+            title = "Evt {}, elecid {}".format(evt, sensors[s][0])
+
+            plt.ion()
+            plt.plot(data, drawstyle='steps')
+            plt.ylim(ymin, ymax)
+            plt.title(title)
+            plt.show()
+            _ = raw_input("Press [enter] to continue.")
+            plt.clf()
+
+def plot_file(h5file):
+    h5in = tb.open_file(h5file)
+    if 'pmtrwf' in h5in.root.RD:
+        plot_waveform(h5in.root.RD.pmtrwf, h5in.root.Sensors.DataPMT[:])
+    if 'pmtblr' in h5in.root.RD:
+        plot_waveform(h5in.root.RD.pmtblr, h5in.root.Sensors.DataBLR[:])
+    if 'sipmrwf' in h5in.root.RD:
+        plot_waveform(h5in.root.RD.sipmrwf, h5in.root.Sensors.DataSiPM[:])
+
+plot_file(results.output)
