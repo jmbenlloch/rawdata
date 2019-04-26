6e18cbee77302046eba2d80efa5f4e1658ba410d
jmbenlloch <info@jmbenlloch.net>
2019-04-26 09:12:48 +0200

    Decode whole file

--- decode_huffman.cc
+++ decode_huffman.cc
@@ -28,7 +28,7 @@ int read_data(unsigned char * data, int position){
 	tmp = tmp | data[position + 1] << 16;
 	tmp = tmp | data[position + 2] <<  8;
 	tmp = tmp | data[position + 3];
-	printf("position: %d, tmp: 0x%08x\n", position, tmp);
+	// printf("position: %d, tmp: 0x%08x\n", position, tmp);
 
 	return tmp;
 }
@@ -48,15 +48,19 @@ void print_huffman(Huffman * huffman, int code){
 
 short int decode_huffman(Huffman * huffman, int code, int position, int * result){
 	int bit = CheckBit(code, position);
+//	printf("pos: %d, bit: %d\n", position, bit);
+
 	int final_pos = position;
 
 	if(! (huffman->next[0] || huffman->next[1])){
 		*result = huffman->value;
 	}else{
-		final_pos = decode_huffman(huffman->next[bit], code, position+1, result);
+		final_pos = decode_huffman(huffman->next[bit], code, position-1, result);
 	}
+	// printf("code: %d, pos: %d, result: %d\n", code, position, *result);
+	// printf("final_pos: %d\n", final_pos);
 
-	return position;
+	return final_pos;
 }
 
 int parse_huffman_line(std::string &line, Huffman * huffman){
@@ -157,15 +161,15 @@ int main(int argc, char* argv[]){
 				if (bitcount < 0){
 					data[position] = tmp;
 
-					printf("val: 0x%02x\n", tmp);
+					// printf("val: 0x%02x\n", tmp);
 					position += 1;
 					bitcount  = 7;
 					tmp       = 0;
 				}
 
-				if (i > 512){
-					break;
-				}
+				// if (i > 512){
+				//     break;
+				// }
 			}
 		}
 		inFile.close();
@@ -204,17 +208,19 @@ int main(int argc, char* argv[]){
 		printf("%02x\n",            data[i*4+3]);
 	}
 
-	for(int i=0; i<32; i++){
-		printf("\n\n new value, i: %d\n", i);
-		int nbits;
+	int values[12];
+
+	//for(int i=0; i<128; i++){
+	for(int i=0; i<12*400000; i++){
+		// printf("\n\n new value, i: %d\n", i);
 
 		// second half of the word -> load next one
 		if(current_bit < 16){
 			// move as many 8-bit words as needed
 			int nwords = (31 - current_bit) / 8;
 			current_bit += nwords * 8;
-			printf("nwords: %d\n", nwords);
-			printf("current_bits: %d\n", current_bit);
+			// printf("nwords: %d\n", nwords);
+			// printf("current_bits: %d\n", current_bit);
 			position_dec += nwords;
 
 		}
@@ -223,25 +229,32 @@ int main(int argc, char* argv[]){
 
 		int type = CheckBit(itmp, current_bit);
 		current_bit -= 1;
-		printf("current_bit: %d\n", current_bit);
-
-		printf("type: %d\n", type);
+		// printf("current_bit: %d\n", current_bit);
+        //
+		// printf("type: %d\n", type);
 
 		int wfvalue;
 		if(type == 0){
-			printf("current_bit: %d\n", current_bit);
+			// printf("current_bit: %d\n", current_bit);
 			wfvalue = (itmp >> (current_bit - 11)) & 0x0FFF;
-			nbits = 12;
+			current_bit -= 12;
+
+			values[i % 12] = wfvalue;
 		}else{
-			nbits = decode_huffman(&huffman, itmp, current_bit, &wfvalue);
+			current_bit = decode_huffman(&huffman, itmp, current_bit, &wfvalue);
+			values[i % 12] = values[i % 12] + wfvalue;
 		}
 
-		current_bit -= nbits;
+		// printf("current_bits: %d\n", current_bit);
+        //
+		// printf("Value: 0x%04x, %d\n", wfvalue, wfvalue);
 
-		printf("Value: 0x%04x\n", wfvalue);
+		if(i%12 == 0){
+			printf("\n");
+		}
+		//printf("wf[%d]: %d\n", i%12, values[i%12]);
+		printf("%d\t", values[i%12]);
 
-		itmp = 0;
-		
 	}
 
 	return 0;
